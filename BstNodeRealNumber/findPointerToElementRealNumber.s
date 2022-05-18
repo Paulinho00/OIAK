918486241720dae1f88694bea793caf6ae1c28c9
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-05-18 13:05:25 +0200

    Dodanie funkcji wyszukujace wskaznik na l. rzeczywista

---
+++ BstNodeRealNumber/findPointerToElementRealNumber.s
@@ -0,0 +1,73 @@
+.text
+
+.global findPointerToElementRealNumber
+
+//(int* pointerToIntValue, int* pointerToFractionalPart, struct BstNodeInt* root, int bytes, int isSigned)
+findPointerToElementRealNumber:
+pushl %ebp
+movl %ebp, %esp
+pushl %ebx
+
+//Odczyt czesci calkowitej
+movl 8(%ebp), %ebx
+//Odczyt wskaznika na korzen
+movl 16(%ebp), %ecx
+
+nextIteration:
+//Sprawdzenie czy wskaznik jest nullem/
+cmp $0, %eax
+je end
+
+pushl %ecx
+pushl 16(%ecx)
+pushl 24(%ebp)
+pushl 20(%ebp)
+pushl (%ecx)
+pushl 8(%ebp)
+call compareIntsInMemory
+addl $20, %esp
+popl %ecx
+
+cmpl $0, %eax
+je checkFractionalPart
+cmpl $1, %eax
+je rightChild
+cmpl $-1, %eax
+je leftChild
+
+
+//Sprawdzenie czesci ulamkowej
+checkFractionalPart:
+pushl %ecx
+pushl 16(%ecx)
+pushl 24(%ebp)
+pushl 20(%ebp)
+pushl 4(%ecx)
+pushl 12(%ebp)
+call compareIntsInMemory
+addl $20, %esp
+popl %ecx
+
+cmpl $0, %eax
+je end
+cmpl $1, %eax
+je rightChild
+cmpl $-1, %eax
+je leftChild
+
+//Wywolanie kolejnej iteracji petli dla prawego dziecka
+rightChild:
+mov 12(%ecx), %ecx
+jmp nextIteration
+
+//Wywolanie kolejnej iteracji petli dla lewego dziecka
+leftChild:
+mov 8(%ecx), %ecx
+jmp nextIteration
+
+
+end:
+movl %ecx, %eax
+popl %ebx
+popl %ebp
+ret
