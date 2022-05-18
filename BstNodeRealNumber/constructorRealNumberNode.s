06734f0b5ac94341529762c39969e95d867716ff
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-05-18 12:40:22 +0200

    Dodanie konstruktora liczb rzeczywistych

---
+++ BstNodeRealNumber/constructorRealNumberNode.s
@@ -0,0 +1,47 @@
+.text
+
+.global constructorRealNumberNode
+
+//(int* pointerToIntValue, int* pointerToFractionalPart, struct BstNodeInt* parent, int* count, int isSigned)
+constructorRealNumberNode:
+pushl %ebp
+movl %esp, %ebp
+pushl %ebx
+
+//Alokacja pamieci na strukture
+pushl $24
+call malloc
+add $4, %esp
+movl %eax, %ebx
+
+//Zapisanie wskaznika na czesc calkowita
+movl 8(%ebp), %eax
+movl %eax, (%ebx)
+
+//Zapisanie wskaznika na czesc ulamkowa
+movl 12(%ebp), %eax
+movl %eax, 4(%ebx)
+
+//Ustawienie null na wskazniki na dzieci nowego elementu
+movl $0, 8(%ebx)
+movl $0, 12(%ebx)
+
+//Zapisanie wskaznika na rodzica
+movl 12(%ebp), %eax
+movl %eax, 16(%ebx)
+
+//Zapisanie informacji o znaku liczby
+movl 20(%ebp), %eax
+movl %eax, 20(%ebx)
+
+//Inkrementacja licznika wezlow
+movl 16(%ebp), %ecx
+incl (%ecx)
+
+movl %ebx, %eax
+popl %ebx
+popl %ebp
+ret
+
+
+
